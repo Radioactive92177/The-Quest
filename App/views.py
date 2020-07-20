399f5a75a9876f93a5949d397351ec221a407a8e
RadioActive92177 <Raju.11811924@lpu.in>
2020-07-20 10:49:14 +0530

    Formatted server initializer

---
+++ App/views.py
@@ -0,0 +1,29 @@
+from App import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/singup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/forgotPass')
+def forgotPass():
+    return render_template('forgotPass.html')
+
+@app.route('/quests')
+def quests():
+    return render_template('quests.html')
+
